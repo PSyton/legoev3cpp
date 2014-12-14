7eadd5ed172af2fce2a47504f75321f6bc17114f
dsjove <dave.r-jove@swbell.net>
2014-12-14 11:57:40 -0600

    Initial Commit

---
+++ JovesLanding/Jove's Landing/Jove's Landing/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  Jove's Landing
+//
+//  Created by David Giovannini on 12/14/14.
+//  Copyright (c) 2014 Software by Jove. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ JovesLanding/Jove's Landing/Jove's Landing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Jove's Landing
+//
+//  Created by David Giovannini on 12/14/14.
+//  Copyright (c) 2014 Software by Jove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
